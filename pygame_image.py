edac94282a7b892ac6f29ccf2a75f384381d4735
C0B22007 <c0b2200787@edu.teu.ac.jp>
2023-11-21 17:09:22 +0900

    背景きれいになった

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,12 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習１
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+    b = pg.transform.flip(bg_img,True,False)
     num=[]
     j=[0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1]
     for i in j:
         a = pg.transform.rotozoom(kk_img,i,1)
         num.append(a)
-    
-    print(num)
         
     
 
@@ -23,13 +22,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr%1600
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0]) #練習４
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(b, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(num[tmr%20],[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(50)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
